36aaaa8a01aa90d12d96f926703fae4885176f7a
Aditya Pathak <adityapathak@Administrators-MacBook-Pro.local>
2022-04-15 17:56:16 +0530

    quiz game v1

--- Quiz-Game/main.go
+++ Quiz-Game/main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	service.StartGame()
+	service.LoadGame()
 }
--- Quiz-Game/service/startgame_service.go
+++ Quiz-Game/service/startgame_service.go
@@ -1,13 +1,18 @@
 package service
 
 import (
+	"bufio"
 	"encoding/csv"
 	"flag"
 	"fmt"
 	"os"
 )
 
-func StartGame() {
+type problem struct {
+	q, a string
+}
+
+func LoadGame() {
 	csvFileName := flag.String("csv", "../Quiz-Game/data/questions.csv", "questions file in form of csv")
 	flag.Parse()
 	file, err := os.Open(*csvFileName)
@@ -16,5 +21,33 @@ func StartGame() {
 		os.Exit(1)
 	}
 	r := csv.NewReader(file)
-	fmt.Println(r)
+	lines, err := r.ReadAll()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	problems := ParseLines(lines)
+	fs := StartGame(problems)
+	fmt.Printf("your score is %d out of %d\n", fs, len(problems))
+}
+
+func ParseLines(lines [][]string) (pbs []problem) {
+	for _, line := range lines {
+		var p problem
+		p.q, p.a = line[0], line[1]
+		pbs = append(pbs, p)
+	}
+	return
+}
+
+func StartGame(pbs []problem) (finalScore int) {
+	in := bufio.NewScanner(os.Stdin)
+	for _, pb := range pbs {
+		fmt.Printf("%s: ", pb.q)
+		in.Scan()
+		if in.Text() == pb.a {
+			finalScore++
+		}
+	}
+	return
 }
